48d2a62663b8ab20b1efa9be81b5cf0931975c0f
Arpis <106545039+Arpis-Project@users.noreply.github.com>
2025-01-14 10:43:33 -0300

    actualizacion componente servipag
    componente servipag con validacion

--- rentdesk/components/servipag/views/servipag.php
+++ rentdesk/components/servipag/views/servipag.php
@@ -5,22 +5,19 @@
 
                 <div class="d-flex">
                     <div class="p-2 w-100">
-                        <div class="mb-3">
                             <label for="formFile" class="form-label">Subir archivo</label>
                             <input class="form-control" type="file" id="formFile" name="file" required>
-                        </div>
                     </div>
                     <div class="p-2 flex-shrink-1">
-                        <button type="submit" class="btn btn-primary">Subir y Procesar</button>
+                        <div class="mt-3"> 
+                            <button type="submit" class="btn btn-primary">Subir</button>
+                        </div>
                     </div>
                 </div>
 
             </form>
 
-
-            <button id="btnSeleccionarTodo" onclick="seleccionarTododeseleccionarTodo()" class="btn btn-success">Seleccionar Todo</button>
-            <button id="btnDeseleccionarTodo" onclick="seleccionarTododeseleccionarTodo()" class="btn btn-danger">Deseleccionar Todo</button>
-            <button class="btn btn-dark" onclick="ProcesarListado()">Procesar</button>
+               <button class="btn btn-dark" onclick="ProcesarListado()">Procesar Pagos</button> 
 
         </div>
     </div>
@@ -32,16 +29,16 @@
             <table id="servipagTable" class="table caption-top">
                 <caption>Servipag</caption>
                 <thead>
-                    <tr>
+                    <tr>    
+                        <td>Nro</td>
                         <td>Rut</td>
+                        <td>ID propiedad</td>
                         <td>Direccion</td>
                         <td>Contrato</td>
                         <td>fecha pago</td>
                         <td>Arriendo</td>
                         <td>Monto Pagado</td>
-                        <td>UF a PESOS</td>
                         <td>Diferencia</td>
-                        <td>procesar</td>
                     </tr>
                 </thead>
                 <tbody>
